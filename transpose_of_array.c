5d4305a08f08cce6093778e1f27b0b369855e202
Ronil Biswas <66785801+RonBis@users.noreply.github.com>
2022-08-31 18:15:42 +0000

    transpose of an array

---
+++ transpose_of_array.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() {
+  int arr[3][3];
+  for (int i = 0; i < 3; i++) {
+    for (int j = 0; j < 3; j++) {
+      printf("ENTER THE VAUES IN %d AND %d POSITIONS: \n", i + 1, j + 1);
+
+      scanf("%d", &arr[i][j]);
+    }
+  }
+  for (int i = 0; i < 3; i++) {
+    for (int j = 0; j < 3; j++) {
+      printf("\t%d", arr[i][j]);
+    }
+    printf("\n");
+  }
+
+  int arrT[3][3];
+  for (int j = 0; j < 3; j++) {
+    for (int i = 0; i < 3; i++) {
+      arrT[j][i] = arr[i][j];
+      printf("\t%d", arrT[j][i]);
+    }
+    printf("\n");
+  }
+
+  return 0;
+}
